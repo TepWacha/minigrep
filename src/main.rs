f57e5de0624d6fc1d113fd41ea38989397364d52
Tep Wacharotayangoon <tep.w@kbtg.tech>
2023-07-01 18:01:56 +0700

    Fully functional minigrep

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::env;
+use std::process;
+use minigrep::{Config,run};
 fn main() {
-    println!("Hello, world!");
+    let args : Vec<String> = env::args().collect();
+    let config = Config::build(&args).unwrap_or_else(|err: &str| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+    if let Err(e) = run(config){
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    };
 }
