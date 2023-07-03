cff57c7567033a1a8d7198c6071ce42a316ba8f6
Tep Wacharotayangoon <tep.w@kbtg.tech>
2023-07-03 18:34:42 +0700

    Upgraded with iterator

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,7 @@ use std::env;
 use std::process;
 use minigrep::{Config,run};
 fn main() {
-    let args : Vec<String> = env::args().collect();
-    let config = Config::build(&args).unwrap_or_else(|err: &str| {
+    let config = Config::build(env::args()).unwrap_or_else(|err: &str| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
