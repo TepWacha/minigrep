7b739d4eb9957b88c4ea9b6dd5dc4fb8ef6be785
Tep Wacharotayangoon <tep.w@kbtg.tech>
2023-07-04 18:02:23 +0700

    update lib name

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::env;
 use std::process;
-use minigrep::{Config,run};
+use tepwgrep::{Config,run};
 fn main() {
     let config = Config::build(env::args()).unwrap_or_else(|err: &str| {
         eprintln!("Problem parsing arguments: {err}");
